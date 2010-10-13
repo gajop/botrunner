d6df1e2c31d5d1473be52a9c427e031d28255a87
Gajo Petrovic <gajop@uns.ac.rs>
2010-10-13 07:21:45 +0200

    seperating from the springgrid project

--- webserver/website/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file's presence lets us import files from this directory, like:
-# import utils.foo as foo
-
-# This lets us do 'from utils import *':
-__all__ = ['dates','formhelper','htmlformshelper','stringhelper','tester','filehelper', 'webpage','listhelper','openidhelper','cgihelper']
-
